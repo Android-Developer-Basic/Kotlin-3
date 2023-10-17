965a552f7e9022471e7c63827559366d600e14fa
Alina <alinavik93@gmail.com>
2023-10-17 12:26:41 +0500

    2 задание

--- src/main/kotlin/ru/otus/homework/hw3.kt
+++ src/main/kotlin/ru/otus/homework/hw3.kt
@@ -4,9 +4,19 @@ fun main () {
     val result = sum(1, 2, 3, 4, 5)
     println(result)
 
+    println("Задание 2")
+    val res1 = mergeLines("привет", "котлин", sep = '-')
+    println(res1)
+
+    println("Задание 3")
+
 
 }
 
 fun sum(first: Int, second: Int, vararg additional: Int): Int {
     return first + second + additional.sum()
+}
+
+fun mergeLines(vararg lines: String, sep: Char = ' '): String {
+    return lines.joinToString(separator = sep.toString())
 }
