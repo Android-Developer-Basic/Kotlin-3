b5258dcf72b10daf09b0e2aac0e617ee9b11e2a8
Ed_Krupenin <ed.krupenin@gmail.com>
2023-10-20 21:31:45 +0300

    Home work 3

--- src/test/kotlin/ru/otus/homework/FunctionsTest.kt
+++ src/test/kotlin/ru/otus/homework/FunctionsTest.kt
@@ -11,4 +11,23 @@ class FunctionsTest {
             calculate(1, 2)
         )
     }
+
+    /**
+     * ## 3. Тестовая функция для пункта №2
+     * Напишите функцию-тест для первого пункта. Проверьте следующие условия для строк `str1` и `str2` и `str3`:
+     *
+     * - Если `Char` не передан, ваша функция должна вернуть: `str1 str2 str3`
+     * - Если `Char` задан как `,`, ваша функция должна вернуть: `str1,str2,str3`
+     * */
+    @Test
+    fun forNumberTwo() {
+        Assertions.assertEquals(
+            "str1 str2 str3",
+            numberTwo("str1", "str2", "str3")
+        )
+        Assertions.assertEquals(
+            "str1,str2,str3",
+            numberTwo("str1", "str2", "str3", c = ',')
+        )
+    }
 }
