cf79e7e7500cb50713c02320527fa7cb8ed0765f
Galina <118059223+Galinnka@users.noreply.github.com>
2023-11-04 18:01:01 +0300

    Homework03-2

--- src/main/kotlin/ru/otus/homework/functions.kt
+++ src/main/kotlin/ru/otus/homework/functions.kt
@@ -2,20 +2,16 @@ package ru.otus.homework
 
 import java.time.LocalDate
 
- fun main(){
-    val ssum = intArrayOf(2, 3, 2)
-    printArg(5, 10, 2, 3)
-    printArg(5, 10, 2, 5, 10, 50)
-    printArg(5, 10, *ssum) //с использованием оператора *
+fun con(vararg string: String, ch: Char = ' '): String {
+    val ch1: String = ch.toString()
+    var result = ""
+    string.forEach { result = result.plus(it).plus(ch1) }
+    println(result)
+    return result
 }
 
+fun main() {
+    con("Hello", "Privet", "Ciao", ch='+')
+    con("Hello", "Privet", "Ciao")
 
-fun printArg(num1: Int, num2: Int, vararg num3: Int) {
-    var result = 0
-    for (nums in num3)
-        result += nums
-    //println("Summ element $result")
-    val a = result
-    val sum = num1 + num2 + a
-    println("Sum element $sum")
 }
