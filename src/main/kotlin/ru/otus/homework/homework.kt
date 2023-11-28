47d79ad22e3fe546ddb2565357e2ea5cfae83cb2
dmilkov97 <denismilkov1@yandex.ru>
2023-11-28 23:24:10 +0700

    Релизовал второй пункт дз

--- src/main/kotlin/ru/otus/homework/homework.kt
+++ src/main/kotlin/ru/otus/homework/homework.kt
@@ -2,8 +2,8 @@ package ru.otus.homework
 
 fun main() {
     println(GetSumm(1,2,5,6,7))
-    println(GetString("asdsd", "tttt", "aaaa", ch ='+'))
-    println(GetString("asdsd", "tttt", "aaaa"))
+    println(getString("asdsd", "tttt", "aaaa", ch ='+'))
+    println(getString("asdsd", "tttt", "aaaa"))
 }
 
 fun GetSumm(num1:Int, num2:Int, vararg nums: Int) : Int {
@@ -14,7 +14,7 @@ fun GetSumm(num1:Int, num2:Int, vararg nums: Int) : Int {
     return sum
 }
 
-fun GetString(vararg strings: String, ch:Char = ' '): String {
+fun getString(vararg strings: String, ch:Char = ' '): String {
     var result:String = ""
     for (str in strings) {
         result = result + str + ch
