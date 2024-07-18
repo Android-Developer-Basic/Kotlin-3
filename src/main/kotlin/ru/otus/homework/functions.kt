2ee8d705a3385b42a4bb1d001fb1239cdb9b2879
crayfish1975 <sermin@bk.ru>
2024-07-18 13:20:40 +0400

    fun2

--- src/main/kotlin/ru/otus/homework/functions.kt
+++ src/main/kotlin/ru/otus/homework/functions.kt
@@ -4,10 +4,15 @@ package ru.otus.homework
 
 fun main() {
     println(calculate(1,2, 3, 4))
+    println(concatenate("Never", "gonna", "give", "you", "up!"))
 }
 
 fun calculate(num1: Int, num2: Int, vararg nums: Int): String {
     var sum = num1 + num2
     nums.forEach { sum += it }
     return "$num1 + $num2 + ${nums.joinToString(" + ")} = $sum"
+}
+
+fun concatenate(vararg lines: String, liner: Char = ' '): String {
+    return lines.joinToString("$liner")
 }
