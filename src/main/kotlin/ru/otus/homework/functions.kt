72f9d0d8383720b185e495661e4dc84db36942a9
crayfish1975 <sermin@bk.ru>
2024-07-18 15:55:00 +0400

    fun4

--- src/main/kotlin/ru/otus/homework/functions.kt
+++ src/main/kotlin/ru/otus/homework/functions.kt
@@ -1,10 +1,14 @@
 package ru.otus.homework
 
+import kotlin.time.Duration
+import kotlin.time.measureTime
+
 //import java.time.LocalDate
 
 fun main() {
     println(calculate(1,2, 3, 4))
     println(concatenate("Never", "gonna", "give", "you", "up!"))
+    println(executionTime { someFunction() })
 }
 
 fun calculate(num1: Int, num2: Int, vararg nums: Int): String {
@@ -15,4 +19,14 @@ fun calculate(num1: Int, num2: Int, vararg nums: Int): String {
 
 fun concatenate(vararg lines: String, liner: Char = ' '): String {
     return lines.joinToString("$liner")
+}
+
+fun executionTime(operation: () -> Unit): Duration {
+    return measureTime(operation)
+}
+
+fun someFunction() {
+    var n: Long = 1
+    for (i in 1..10_000_000_000)
+        n *= i
 }
