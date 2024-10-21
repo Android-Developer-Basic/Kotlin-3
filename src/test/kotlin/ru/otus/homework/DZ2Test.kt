afcbf38949efbb40c4f70cc95b371f3da22b271a
Ilfirinon <Ilfirinon@yandex.com>
2024-10-21 16:21:08 +0800

    Add Test to DZ2

---
+++ src/test/kotlin/ru/otus/homework/DZ2Test.kt
@@ -0,0 +1,29 @@
+package ru.otus.homework
+
+
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class StringCombinationTest {
+
+    @Test
+    fun `testStringCombinationWithDefaultSeparator`() {
+        val str1 = "Привет"
+        val str2 = "мир"
+        val str3 = "Kotlin"
+        val expected = "$str1 $str2 $str3"
+        val actual = stringCombination(str1, str2, str3)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `testStringCombinationWithCustomSeparator`() {
+        val str1 = "Привет"
+        val str2 = "мир"
+        val str3 = "Kotlin"
+        val expected = "$str1,$str2,$str3"
+        val actual = stringCombination(str1, str2, str3, separator = ',')
+        assertEquals(expected, actual)
+    }
+}
