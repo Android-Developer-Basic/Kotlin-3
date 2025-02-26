71a88dd3309c44426877e04bf579c0171ad0a928
Adel Khaziakhmetov <adel.khaziakhmetov@shtrafy-gibdd.ru>
2025-02-26 17:54:23 +0300

    adel khaziakhmetov homework 2

--- src/test/kotlin/ru/otus/homework/FunctionsTest.kt
+++ src/test/kotlin/ru/otus/homework/FunctionsTest.kt
@@ -5,10 +5,24 @@ import org.junit.jupiter.api.Test
 
 class FunctionsTest {
     @Test
-    fun calculationTest() {
-        Assertions.assertEquals(
-            "1 + 2 = 3 Огурцов",
-            calculate(1, 2)
-        )
+    fun stringJoinTest() {
+        val firstResult = stringJoin("str1","str2","str3")
+        val secondResult = stringJoin("str1","str2","str3", separator = ',')
+
+        Assertions.assertEquals(firstResult, "str1 str2 str3")
+        Assertions.assertEquals(secondResult, "str1,str2,str3")
+    }
+
+    @Test
+    fun timeFunTest() {
+        fun tempFun(n: Int) {
+            for (i in 0..n) {
+                println(1111 * i)
+            }
+        }
+
+        val result = time(func = ::tempFun)
+
+        Assertions.assertEquals(result,9)
     }
 }
