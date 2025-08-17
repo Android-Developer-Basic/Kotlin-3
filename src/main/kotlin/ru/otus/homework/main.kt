03b11c6b63229d8f698c853cd4d8c01ac3d26aad
v_kopychko <v_kopychko@inpas.ru>
2025-08-18 02:31:15 +0300

    Реализация четвертой функции

--- src/main/kotlin/ru/otus/homework/main.kt
+++ src/main/kotlin/ru/otus/homework/main.kt
@@ -1,5 +1,7 @@
 package ru.otus.homework
 
+import kotlin.system.measureTimeMillis
+
 fun fun1MandatoryAndOptionalParams(numberOne: Int, numberTwo: Int, vararg additionalNumber: Int): Int {
     var sum = 0
     for(i in additionalNumber)
@@ -14,7 +16,24 @@ fun fun2OptionalAndPositionalParams(vararg additionalStr: String, ch: Char=' '):
     return result.dropLast(1)
 }
 
+fun sum(a: Int, b: Int){
+    var sum : Long = 0
+    repeat(a) {
+        repeat(b) {
+            sum += a+b
+        }
+    }
+}
+
+fun fun4Time(numberOne: Int, numberTwo: Int, myFunction: (Int, Int)->Unit): Long {
+    val time = measureTimeMillis {
+        myFunction(numberOne, numberTwo)
+    }
+    return time
+}
+
 fun main() {
     println(fun1MandatoryAndOptionalParams(8, 45, 10,20))
     println(fun2OptionalAndPositionalParams("one", "two", "free", "four", "five", ch= '*'))
+    println(fun4Time(100000,200000, :: sum))
 }
