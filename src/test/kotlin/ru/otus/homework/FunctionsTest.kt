d3fd42aed274bb804e4102c373f15c6765689abc
Сальников Евгений Владимирович <salnikov_e_v@m00office0207.local>
2025-08-18 21:17:56 +0300

    home work 3

--- src/test/kotlin/ru/otus/homework/FunctionsTest.kt
+++ src/test/kotlin/ru/otus/homework/FunctionsTest.kt
@@ -11,4 +11,26 @@ class FunctionsTest {
             calculate(1, 2)
         )
     }
+
+    @Test
+    fun sumOfIntTest() {
+        Assertions.assertEquals(10, sumOfInt(3, 7))
+        Assertions.assertEquals(20, sumOfInt(3, 7, 10))
+        Assertions.assertEquals(40, sumOfInt(3, 7, 11, 19))
+        Assertions.assertEquals(-19, sumOfInt(3, 7, -10, -19))
+    }
+
+    @Test
+    fun stringCombineTest() {
+        Assertions.assertEquals("a b c", stringCombine("a", "b", "c"))
+        Assertions.assertEquals("a,b,c", stringCombine("a", "b", "c", delimiter = ','))
+        Assertions.assertEquals("a:b:c", stringCombine("a", "b", "c", delimiter = ':'))
+    }
+
+    @Test
+    fun getExecutionTimeMillisTest() {
+        Assertions.assertTrue { 100 < getExecutionTimeMillis { longFunc() } }
+        Assertions.assertTrue { 200 < getExecutionTimeMillis { longFunc(200) } }
+        Assertions.assertTrue { 200 > getExecutionTimeMillis { longFunc(100) } }
+    }
 }
