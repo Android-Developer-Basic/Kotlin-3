492534b74caccd8a6b5519a369b00d25d7336680
Karavaev <karavaev@inpas.ru>
2025-08-26 14:33:21 +0300

    Homework_Kotlin#3

--- src/test/kotlin/ru/otus/homework/FunctionsTest.kt
+++ src/test/kotlin/ru/otus/homework/FunctionsTest.kt
@@ -2,13 +2,20 @@ package ru.otus.homework
 
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class FunctionsTest {
     @Test
-    fun calculationTest() {
+    fun task2Test() {
+
         Assertions.assertEquals(
-            "1 + 2 = 3 Огурцов",
-            calculate(1, 2)
+            "str1 str2 str3",
+            task_2("str1", "str2", "str3")
+        )
+
+        Assertions.assertEquals(
+            "str1,str2,str3",
+            task_2("str1", "str2", "str3", separ = ',')
         )
     }
 }
